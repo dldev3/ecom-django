88174a2cdd75a2d14d956c886d35f501c5858ad4
d_liyanage <admliyanage@gmail.com>
2021-02-19 11:34:46 +0530

    init

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+def product(request):
+    return render(request,'product.html')
